259825101028079f5a89136e0f1c6aa11ddbf7a0
pontusalm <pontus.alm@gmail.com>
2022-03-03 17:55:29 +0100

    sdf

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import sqlalchemy
 class ConfigDebug():
    SECRET_KEY = 'nowichange#!1212'
   #  SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Lsafms01a!@localhost/pabanken'
-   SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root12345:Hejsan123#@pontus1918.mysql.database.azure.com/pontus2022'    # File-based SQL database
+   SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root12345:Hejsan123#@pontus1920.mysql.database.azure.com/pontus2024'    # File-based SQL database
    
    SQLALCHEMY_TRACK_MODIFICATION=FALSE
 
